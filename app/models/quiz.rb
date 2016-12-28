176941e3b55069764e12eb73fd626df21efc85af
Matteo Revelli <matteo.revelli@mikamai.com>
2016-12-28 18:15:56 +0100

    finish skeleton of main struttura

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -1,7 +1,7 @@
 class Quiz < ActiveRecord::Base
 
   def sum_score
-    Quiz.last.answer + Quiz.last.score
+    Quiz.last.answer + Quiz.last.score + Quiz.last.question_1 + Quiz.last.question_2 + Quiz.last.question_3
   end
 
 end
